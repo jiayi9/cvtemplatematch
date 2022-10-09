a90874111ad1372d70fb42c4fcfd25bed3091a07
Jiayi Lu <jiayi.lu1@astrazeneca.com>
2022-10-09 09:35:20 +0800

    xxx

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import cv2 as cv
-import cv2
 import numpy as np
-from matplotlib import pyplot as plt
-import pylab
 import os
 from pathlib import Path
 import pandas as pd
+from time import time
 
 
 def list_files_recur(path: str, fmt: str = ".bmp"):
@@ -21,36 +19,50 @@ def list_files_recur(path: str, fmt: str = ".bmp"):
     return [file_paths, file_names]
 
 
-def resize_image(image, n):
-    return cv.resize(image, (int(image.shape[1]/n), int(image.shape[0]/n)))
+def resize_image(image: np.ndarray, n: int):
+    _start = time()
+    resized_image = cv.resize(image, (int(image.shape[1]/n), int(image.shape[0]/n)))
+    run_time = time() - _start
+    print(run_time)
+    return resized_image, run_time
 
 
-def find_reference_point(img: np.ndarray, template: np.ndarray, n:int = 1, method_name: str = 'cv2.TM_CCOEFF'):
+def find_reference_point(img: np.ndarray, template: np.ndarray, n:int = 1, method_name: str = 'cv.TM_CCOEFF'):
     """ Find the top left point as the reference point """
     if n != 1:
-        img = resize_image(img, n)
-        template = resize_image(template, n)
+        img, resize_image_run_time = resize_image(img, n)
+        template, resize_template_run_time = resize_image(template, n)
+    else:
+        resize_image_run_time, resize_template_run_time = 0, 0
 
     method = eval(method_name)
-    w, h = template.shape[0:2]
     # Apply template Matching
-    res = cv2.matchTemplate(img, template, method)
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+    _start = time()
+    res = cv.matchTemplate(img, template, method)
+    run_time = time() - _start
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
     # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-    if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+    if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
         top_left = min_loc
     else:
         top_left = max_loc
     # bottom_right = (top_left[0] + w, top_left[1] + h)
     top_left = [x*n for x in top_left]
-    return top_left
+    return top_left, run_time, resize_image_run_time, resize_template_run_time
 
 
 def add_roi_rect(img, top_left, shape):
     """ Add a rectangle for ROI """
     h, w = shape[0:2]
     bottom_right = (top_left[0] + w, top_left[1] + h)
-    cv.rectangle(img, top_left, bottom_right, (0, 0 ,255), 8) #bgr
+    if len(img.shape) == 3:
+        cv.rectangle(img, top_left, bottom_right, (0, 0, 255), 8) #bgr
+    else:
+        # need the following line to solve this bug
+        img = np.array(img)
+        cv.rectangle(img, top_left, bottom_right, 255, 8)
+
     return img
 
 
@@ -67,9 +79,13 @@ roi_templates = list_files_recur("C:/Temp/roi_study/blister_front/roi_template")
 normal_raw_imgs = list_files_recur("C:/Temp/roi_study/blister_front/normal_raw")[0]
 
 
-resizing_factors = (1, 2)
-method_names = ['cv.TM_CCOEFF']
+#resizing_factors = (2,1)
+#method_names = ['cv.TM_CCOEFF']
 
+# method, n, template, raw_image, ->
+# resizing_time_1, resizing_time_2, color_find_time, color_x, color_y, gray_find_time, gray_x, gray_y
+
+L = []
 
 for method_name in method_names:
 
@@ -79,52 +95,62 @@ for method_name in method_names:
 
         os.makedirs(os.path.join(home_folder, method_name, str(n)), exist_ok=True)
 
-        for template_idx, template_path in enumerate(roi_templates):
+        for template_idx, template_path in enumerate(roi_templates[0:1]):
 
             os.makedirs(os.path.join(home_folder, method_name, str(n), Path(template_path).stem), exist_ok=True)
 
-            template = cv2.imread(template_path)
+            template = cv.imread(template_path)
 
-            for raw_image_idx, raw_image_path in enumerate(normal_raw_imgs):
+            for raw_image_idx, raw_image_path in enumerate(normal_raw_imgs[0:5]):
 
-                img = cv2.imread(raw_image_path)
+                # color
+                img = cv.imread(raw_image_path)
 
-                top_left = find_reference_point(img, template, n, method_name)
-
+                top_left, match_time, resize_time_1, resize_time_2 = find_reference_point(img, template, n, method_name)
                 image_with_roi = add_roi_rect(img, top_left, template.shape)
-
                 save_path = os.path.join(home_folder, method_name, str(n), Path(template_path).stem, Path(raw_image_path).stem + ".png")
+                cv.imwrite(save_path, image_with_roi)
 
-                cv2.imwrite(save_path, image_with_roi)
+                # gray scale
+                _start = time()
+                img_gray = img[:, :, 0]
+                template_gray = template[:, :, 0]
+                gray_time = time() - _start
+
+                top_left_gray, match_time_gray, resize_time_1_gray, resize_time_2_gray = find_reference_point(img_gray, template_gray, n, method_name)
+                image_with_roi_gray = add_roi_rect(img_gray, top_left_gray, template_gray.shape)
+                save_path_gray = os.path.join(home_folder, method_name, str(n), Path(template_path).stem, Path(raw_image_path).stem + "_gray.png")
+                cv.imwrite(save_path_gray, image_with_roi_gray)
+
+                # save information
+                L.append({
+                    'method': method_name,
+                    'n': n,
+
+                    'match_time': match_time,
+                    'resize_time_1': resize_time_1,
+                    'resize_time_2': resize_time_2,
+                    'x': top_left[0],
+                    'y': top_left[1],
+                    'total_time_color': match_time + resize_time_1 + resize_time_2,
+
+                    'gray': gray_time,
+
+                    'match_time_gray': match_time_gray,
+                    'resize_time_1_gray': resize_time_1_gray,
+                    'resize_time_2_gray': resize_time_2_gray,
+                    'x_gray': top_left_gray[0],
+                    'y_gray': top_left_gray[1],
+
+                    'total_time_gray': match_time_gray + resize_time_1_gray + resize_time_2_gray
+                })
 
 
 
-# img = cv2.imread(normal_raw_imgs[2])
-# template = cv2.imread(roi_templates[0])
-#
-# img.shape
-# template.shape
-# top_left = find_reference_point(img, template, n=1, method_name='cv2.TM_CCOEFF')
-#
-# print(top_left)
-#
+                #
+                # print(method_name, n,
+                #       match_time, resize_time_1, resize_time_2, top_left[0], top_left[1] ,"    |   ",  gray_time,
+                #       match_time_gray, resize_time_1_gray, resize_time_2_gray, top_left_gray[0], top_left_gray[1])
 
-
-
-# if __name__ == "__main__":
-#
-#     print(roi_templates)
-
-# file_path = r"C:\Temp\sample\022584b9-02b3-4e8d-ba1f-3a70b985f656.bmp"
-#
-# img = cv.imread(file_path, 0)
-# img_rgb = cv.imread(file_path)
-#
-# img2 = img.copy()
-#
-# template = cv.imread(file_path,0)[600:1600, 90:2350]
-#
-# w, h = template.shape[::-1]
-#
-# plt.imshow(template, cmap = 'gray')
-# pylab.show()
+df = pd.DataFrame(L)
+df.to_csv(os.path.join(home_folder, "summary.csv"), index=False)
