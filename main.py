793bffa1d359622e3dbb7ef7ccff834c8519e33c
Jiayi Lu <jiayi.lu1@astrazeneca.com>
2022-10-09 13:53:52 +0800

    xxx

--- main.py
+++ main.py
@@ -72,9 +72,9 @@ if __name__ == '__main__':
     # factors: method, n, template, raw
 
     # Blister from raw blister
-    # home_folder = "C:/Temp/roi_study_result/blister"
-    # template_folder = "C:/Temp/roi_study/blister_front/roi_template"
-    # image_folder = "C:/Temp/roi_study/blister_front/normal_raw"
+    home_folder = "C:/Temp/roi_study_result/blister"
+    template_folder = "C:/Temp/roi_study/blister_front/roi_template"
+    image_folder = "C:/Temp/roi_study/blister_front/normal_raw"
 
     # Emblossing from carton side
     # home_folder = "C:/Temp/roi_study_result/carton_ocr"
@@ -87,15 +87,16 @@ if __name__ == '__main__':
     # image_folder = "C:/Temp/roi_study/blister_front/normal_raw"
 
     # OCR from blister ROI
-    home_folder = "C:/Temp/roi_study_result/blister_ocr_from_roi"
-    template_folder = "C:/Temp/roi_study/blister_front/ocr_roi_template"
-    image_folder = "C:/Temp/roi_study/blister_front/roi_template"
+    # home_folder = "C:/Temp/roi_study_result/blister_ocr_from_roi"
+    # template_folder = "C:/Temp/roi_study/blister_front/ocr_roi_template"
+    # image_folder = "C:/Temp/roi_study/blister_front/roi_template"
 
+    method_names = ['cv.TM_CCOEFF']
 
-    method_names = ['cv.TM_CCOEFF']#, 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
-                    #'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
+    # method_names = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
+    #                 'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
 
-    resizing_factors = (1, 2, 3, 4, 8, 16, 32, 64, 128)
+    resizing_factors = (1, 2, 3, 4, 8, 16, 32, 64)
 
     # use part of the images
     roi_templates = list_files_recur(template_folder)[0][0:1]
